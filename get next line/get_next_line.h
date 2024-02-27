3af85037757bf4d3b7b84a0f978bdccb45d8b60b
Stefano Alexander Di Puma <stdi-pum@c0c6c3.42berlin.de>
2024-02-27 14:25:40 +0100

    last?

--- get next line/get_next_line.h
+++ get next line/get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: stdi-pum <stdi-pum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:10:30 by stdi-pum          #+#    #+#             */
-/*   Updated: 2024/02/22 19:34:10 by stdi-pum         ###   ########.fr       */
+/*   Updated: 2024/02/27 14:23:17 by stdi-pum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 3
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000
+# endif
 
 # include <limits.h>
 # include <stdarg.h>
@@ -25,15 +25,8 @@
 # include <string.h>
 # include <unistd.h>
 
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
-size_t	ft_strlen(const char *str);
-char    *get_next_line(int fd);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t nmemb, size_t size);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-char    *ft_get_line (int fd, char *buff, int count);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		getlen(char *str);
+char	*ft_strjoin_get_next_line(char *s1, char *s2);
+char	*get_next_line(int fd);
 
 #endif
