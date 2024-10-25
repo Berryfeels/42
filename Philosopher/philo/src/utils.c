04901be4d1f2cd865bc269a0e569ce2974e3d3f1
Stefano Alexander Di Puma <stdi-pum@c3a1c6.42berlin.de>
2024-10-26 00:14:37 +0200

    kl

--- Philosopher/philo/src/utils.c
+++ Philosopher/philo/src/utils.c
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stdi-pum <stdi-pum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/08 18:07:06 by stdi-pum          #+#    #+#             */
-/*   Updated: 2024/10/11 15:28:52 by stdi-pum         ###   ########.fr       */
+/*   Created: 2024/10/11 12:15:08 by stdi-pum          #+#    #+#             */
+/*   Updated: 2024/10/26 00:07:44 by stdi-pum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-int main (int argc, char **argv)
+int	ft_atoi(char *digit)
 {
-	t_args *data;
+	int num;
 
-	data = malloc(sizeof(t_args));
-	if(!data)
-		ft_exit_error(EXIT_ERROR_STRUCT_INIT);	
-	ft_error_check(argc, argv);
-	arg_data_init (data, argv);
-	ft_threads(data);
-	free (data);
-	return 0;
+	if (digit[0]< '0' || digit [0] > '9')
+		ft_exit_error(EXIT_ERROR_ARGS);
+	num = 0;
+	while (*digit)
+	{
+		num = (num * 10) + (*digit - 48); 
+		digit++;
+	} 
+	return (num); 
 }
